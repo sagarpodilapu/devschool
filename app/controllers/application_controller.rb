defcdd9457a899892801ec96326aa88d208e6edd
Podilapu,Sagar <Sagar.Podilapu@cerner.com>
2016-12-02 15:21:14 +0530

    added a bare bootstrap template and Gsuite meta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: "hello, world"
+  def index
+    render "index"
   end
 end
